12c808d5408be4184b997b4bff02f1ce86397ab8
joaquinfer87 <joquinfer100@gmail.com>
2023-02-22 19:58:15 -0300

    nueva rama

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const app = () => {
   return (
     <>
     <NavBar/>
-    <ItemListConteiner gretting ="Bienvenido a Sticker Planet" />
+    <ItemListConteiner gretting ="Bienvenido a gola  Planet" />
     </>
   );
 };
